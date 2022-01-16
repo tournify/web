24c4e10b32aba2aa4075cee9ccd173dc6b8fe5a2
uberswe <m@rkus.io>
2022-01-16 16:31:42 +0100

    Initial localization implementation

---
+++ lang/messages.go
@@ -0,0 +1,126 @@
+package lang
+
+import "github.com/nicksnyder/go-i18n/v2/i18n"
+
+var translationMessages = []i18n.Message{
+	{
+		ID:    "home",
+		Other: "Home",
+	},
+	{
+		ID:    "site_name",
+		Other: "Tournify.io",
+	},
+	{
+		ID:    "create_tournament",
+		Other: "Create Tournament",
+	},
+	{
+		ID:    "dashboard",
+		Other: "Dashboard",
+	},
+	{
+		ID:    "blog",
+		Other: "Blog",
+	},
+	{
+		ID:    "create_blog_post",
+		Other: "Create Blog Post",
+	},
+	{
+		ID:    "admin",
+		Other: "Admin",
+	},
+	{
+		ID:    "logout",
+		Other: "Logout",
+	},
+	{
+		ID:    "register",
+		Other: "Register",
+	},
+	{
+		ID:    "search",
+		Other: "Search",
+	},
+	{
+		ID:    "created_by",
+		Other: "Created by",
+	},
+	{
+		ID:    "privacy_policy",
+		Other: "Privacy Policy",
+	},
+	{
+		ID:    "terms_of_service",
+		Other: "Terms of Service",
+	},
+	{
+		ID:    "all_rights_reserved",
+		Other: "All rights reserved.",
+	},
+	{
+		ID:    "fork_this_project_on",
+		Other: "Fork this project on",
+	},
+	{
+		ID:    "login",
+		Other: "Login",
+	},
+	{
+		ID:    "site_name_first",
+		Other: "Tournify",
+	},
+	{
+		ID:    "site_name_last",
+		Other: ".io",
+	},
+	{
+		ID:    "easy_to_create_tournaments",
+		Other: "Easy to create tournaments",
+	},
+	{
+		ID:    "tournament_name",
+		Other: "Tournament Name",
+	},
+	{
+		ID:    "optional",
+		Other: "Optional",
+	},
+	{
+		ID:    "create",
+		Other: "Create",
+	},
+	{
+		ID:    "subscribe_to_our_newsletter",
+		Other: "Subscribe to our newsletter",
+	},
+	{
+		ID:    "email",
+		Other: "Email",
+	},
+	{
+		ID:    "subscribe",
+		Other: "Subscribe",
+	},
+	{
+		ID:    "no_spam_notice",
+		Other: "If you want to receive updates from us just pop your email in the box. We think that spam is for jerks. And jerks we are not.",
+	},
+	{
+		ID:    "recent_tournaments",
+		Other: "Recent Tournaments",
+	},
+	{
+		ID:    "the_tournament",
+		Other: "The tournament",
+	},
+	{
+		ID:    "was_created",
+		Other: "was created",
+	},
+	{
+		ID:    "ago",
+		Other: "ago",
+	},
+}
--- routes/index.go
+++ routes/index.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"github.com/tournify/web/models"
 	"log"
 	"net/http"
@@ -10,91 +9,10 @@ import (
 
 type IndexData struct {
 	PageData
-	SiteNameFirst            string
-	SiteNameLast             string
-	EasyToCreateTournaments  string
-	TournamentName           string
-	Optional                 string
-	Create                   string
-	SubscribeToOurNewsletter string
-	Email                    string
-	Subscribe                string
-	NoSpamNotice             string
-	RecentTournaments        []models.Tournament
+	RecentTournaments []models.Tournament
 }
 
 func (controller Controller) Index(c *gin.Context) {
-	localize := i18n.NewLocalizer(controller.bundle, domainLanguage(c))
-
-	siteNameFirst, _ := localize.Localize(&i18n.LocalizeConfig{
-		DefaultMessage: &i18n.Message{
-			ID:    "site_name_first",
-			Other: "Tournify",
-		},
-	})
-
-	siteNameLast, _ := localize.Localize(&i18n.LocalizeConfig{
-		DefaultMessage: &i18n.Message{
-			ID:    "site_name_last",
-			Other: ".io",
-		},
-	})
-
-	easyToCreateTournaments, _ := localize.Localize(&i18n.LocalizeConfig{
-		DefaultMessage: &i18n.Message{
-			ID:    "easy_to_create_tournaments",
-			Other: "Easy to create tournaments",
-		},
-	})
-
-	tournamentName, _ := localize.Localize(&i18n.LocalizeConfig{
-		DefaultMessage: &i18n.Message{
-			ID:    "tournament_name",
-			Other: "Tournament name",
-		},
-	})
-
-	optional, _ := localize.Localize(&i18n.LocalizeConfig{
-		DefaultMessage: &i18n.Message{
-			ID:    "optional",
-			Other: "Optional",
-		},
-	})
-
-	create, _ := localize.Localize(&i18n.LocalizeConfig{
-		DefaultMessage: &i18n.Message{
-			ID:    "create",
-			Other: "Create",
-		},
-	})
-
-	subscribeToOurNewsletter, _ := localize.Localize(&i18n.LocalizeConfig{
-		DefaultMessage: &i18n.Message{
-			ID:    "subscribe_to_our_newsletter",
-			Other: "Subscribe to our newsletter",
-		},
-	})
-
-	email, _ := localize.Localize(&i18n.LocalizeConfig{
-		DefaultMessage: &i18n.Message{
-			ID:    "email",
-			Other: "Email",
-		},
-	})
-
-	subscribe, _ := localize.Localize(&i18n.LocalizeConfig{
-		DefaultMessage: &i18n.Message{
-			ID:    "subscribe",
-			Other: "Subscribe",
-		},
-	})
-
-	noSpamNotice, _ := localize.Localize(&i18n.LocalizeConfig{
-		DefaultMessage: &i18n.Message{
-			ID:    "no_spam_notice",
-			Other: "If you want to receive updates from us just pop your email in the box. We think that spam is for jerks. And jerks we are not.",
-		},
-	})
 
 	// Select 10 most recent tournaments
 	var recentTournaments []models.Tournament
@@ -105,18 +23,8 @@ func (controller Controller) Index(c *gin.Context) {
 	}
 
 	id := IndexData{
-		PageData:                 controller.defaultPageData(c),
-		SiteNameFirst:            siteNameFirst,
-		SiteNameLast:             siteNameLast,
-		EasyToCreateTournaments:  easyToCreateTournaments,
-		TournamentName:           tournamentName,
-		Optional:                 optional,
-		Create:                   create,
-		SubscribeToOurNewsletter: subscribeToOurNewsletter,
-		Email:                    email,
-		Subscribe:                subscribe,
-		NoSpamNotice:             noSpamNotice,
-		RecentTournaments:        recentTournaments,
+		PageData:          controller.defaultPageData(c),
+		RecentTournaments: recentTournaments,
 	}
 	c.HTML(http.StatusOK, "index.html", id)
 }
